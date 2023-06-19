7216689f2810b92a3e9a7b78bb9b40a1fc0d11a4
CamillePB <camille.pedro.bueno@hotmail.com>
2023-06-19 16:21:32 -0300

    prisma sem user e finalização das telas

--- app/index.tsx
+++ app/index.tsx
@@ -1,77 +1,45 @@
 /* eslint-disable prettier/prettier */
-import { StatusBar } from 'expo-status-bar'
-import { Text, ImageBackground, TouchableOpacity, View } from 'react-native'
-import blurbg from '../src/assets/bg-blur.png'
-import Stripes from '../src/assets/stripes.svg'
+import { Text, TouchableOpacity, View } from 'react-native'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { useRouter } from 'expo-router'
-
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-
-import {
-  BaiJamjuree_700Bold
-} from '@expo-google-fonts/bai-jamjuree'
-import { styled } from 'nativewind';
-import React from 'react';
-// import React = require('react');
-
-const StyledStripes = styled(Stripes)
+import React from 'react'
 
 export default function App() {
   const router = useRouter()
 
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold
-  })
-
-  //nao mostarr a interface até as fontes carregarem
-  if(!hasLoadedFonts){
-    return null
-  }
 
   async function goMemories() {
   router.push('/memories')
   }
 
   return (
-    <ImageBackground source={blurbg} 
-    className='bg-gray-950 items-center justify-center flex-1 relative'
-    imageStyle={{position:'absolute', left:'-100%'}}>
-     <StyledStripes className='absolute left-2'/>
+    <View className="flex-1 items-center px-8 py-10">
+    <View className="flex-1 items-center justify-center gap-6">
+      <NLWLogo />
 
-     <View className="flex-1 items-center justify-center gap-6">
-        <NLWLogo />
-       
-        <View className="space-y-2">
-          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
-            Sua cápsula do tempo
-          </Text>
-          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
-            Colecione momentos marcantes da sua jornada e compartilhe (se
-            quiser) com o mundo!
-          </Text>
-        </View>
+      <View className="space-y-2">
+        <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+          Sua cápsula do tempo
+        </Text>
+        <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+          Colecione momentos marcantes da sua jornada e compartilhe (se
+          quiser) com o mundo!
+        </Text>
+      </View>
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full px-5 py-2 bg-orange-300 "
           onPress={() => goMemories()}
         >
-          <Text className="font-alt text-sm uppercase text-black">
+           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembrança
           </Text>
         </TouchableOpacity>
-        
-        </View>
-        <Text className="text-center font-body text-sm leading-relaxed text-gray-200 pb-1">
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   );
 }
